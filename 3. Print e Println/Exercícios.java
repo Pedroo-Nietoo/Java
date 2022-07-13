18ddf7c38425f1443e0b28bf6f9d86379cf67a80
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-13 12:37:25 -0300

    Create Exercícios.java

---
+++ 3. Print e Println/Exercícios.java
@@ -0,0 +1,12 @@
+public class Exercícios {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+    int c = a*b/3;
+    
+    //Faça com que o resultado de "c" apareça no terminal".
+    
+    String user = "@Pedroo-Nietoo";
+    //Faça com que a frase "Seu usuário do GitHub é: " apareça unto com o a variável "user".
+  }
+}
