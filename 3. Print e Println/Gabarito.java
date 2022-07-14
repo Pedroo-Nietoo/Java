d22cb0a5712485f637b80d121bc1764235e7d6c4
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-14 07:36:20 -0300

    Create Gabarito.java

---
+++ 3. Print e Println/Gabarito.java
@@ -0,0 +1,12 @@
+public class Exercícios {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+    int c = a*b/3;
+    
+    System.out.println(c);
+    
+    String user = "@Pedroo-Nietoo";
+    System.out.print("Seu usuário do GitHub é: "+user);
+  }
+}
