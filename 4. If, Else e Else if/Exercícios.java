2745886890c4ba80f3f9f5aa6658441107f51d02
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-17 12:03:40 -0300

    Update Exercícios.java

--- 4. If, Else e Else if/Exercícios.java
+++ 4. If, Else e Else if/Exercícios.java
@@ -3,9 +3,9 @@ public class Exercícios{
     /*
     Crie uma variável chamada idade e atribua um valor inteiro a ela.
     
-    Se a idade for menor do que 5, printe "Você é uma criança";
-    Caso contrário, se a idade for menor do que 17, printe "Você é um adolescente";
-    Caso contrário, printe "Você é maior de idade".
+    Se a idade for menor do que 5, printe "Você é uma criança.";
+    Caso contrário, se a idade for menor ou igual a 17, printe "Você é um adolescente.";
+    Caso contrário, printe "Você é maior de idade.".
     */
   }
 }
