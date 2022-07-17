c34a209b38a3c663cf3d34f97685a5873d3204b0
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-17 12:03:12 -0300

    Update Gabarito.java

--- 4. If, Else e Else if/Gabarito.java
+++ 4. If, Else e Else if/Gabarito.java
@@ -1,1 +1,15 @@
-A
+public class Gabarito{
+  public static void main(String[] args){
+    int idade = 15;
+    
+    if(idade<5){
+      System.out.println("Você é uma criança");
+    }
+    else if(idade<=17){
+      System.out.println("Você é um adolescente");
+    }
+    else{
+      System.out.println("Você é maior de idade");
+    }
+  }
+}
